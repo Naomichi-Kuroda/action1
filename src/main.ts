e509b609b1a803a8c01b05c41a1cbd073c495c4e
Naomichi Kuroda <naomichi.kuroda.jp@gmail.com>
2020-09-18 15:24:18 +0900

    comment test

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    core.info('start')
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
